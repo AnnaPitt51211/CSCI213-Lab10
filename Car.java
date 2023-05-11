2ebf429681aecaeeef8dcf2584ad5e9d666834e6
Anna Pitt <annapitt@go.rmc.edu>
2023-05-11 14:14:47 -0400

    README and Car commit

--- Car.java
+++ Car.java
@@ -21,31 +21,39 @@ public class Car extends Vehicle
 	
 	public int getDoors()
 	{
+		// Returns the Car's door count
 		return this.doorCount;
 	}
 	
 	public int getPassengers()
 	{
+		// Returns the Car's passenger count
 		return this.passengerCount;
 	}
 	
 	@Override
 	public String toString()
-	{		
+	{	
+		// Formats the String
 		String result = String.format("%d-door %s %s with license %s.", this.doorCount, super.getMake(), super.getModel(), super.getPlate());
+		
+		// Returns the String result
 		return result;
 	}
 	
 	@Override
 	public boolean equals(Object other)
 	{
+		// Returns false if the object passed in isn't of the Car type
 		if(!(other instanceof Car))
 		{
 			return false;
 		}
 		
+		// Casts the object passed in to the Car type
 		Car otherCar = (Car) other;
 		
+		// If the Cars are equal, returns True
 		if(this.doorCount == otherCar.doorCount)
 		{
 			if(this.passengerCount == otherCar.passengerCount)
@@ -54,6 +62,7 @@ public class Car extends Vehicle
 			}
 		}
 		
+		// Otherwise, returns False
 		return false;
 	}
 	
